88b7a5b960744848c218a15889d7bca4b35dc257
pkamm <sprizzla@gmail.com>
2015-08-28 20:17:18 -0400

    Initial Commit

---
+++ Spotlight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Spotlight
+//
+//  Created by Peter Kamm on 8/28/15.
+//  Copyright (c) 2015 Spotlight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Spotlight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Spotlight
+//
+//  Created by Peter Kamm on 8/28/15.
+//  Copyright (c) 2015 Spotlight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
